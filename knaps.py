9e923c4f3827976325f3d4cd87d1c5670b407613
dalihzulfaa <112567306+dalihzulfaa@users.noreply.github.com>
2022-11-30 21:24:46 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -105,7 +105,7 @@ with modeling:
         print('recall_Naive Bayes: %.3f' %recall)
         print('f1-score_Naive Bayes : %.3f' %f1)
         st.success(accuracy)
-        label_nb = pd.DataFrame(
+        label_nb = pd.DataFrame( 
         data={'Label Test': y_test, 'Label Predict': y_pred})
         label_nb
         
